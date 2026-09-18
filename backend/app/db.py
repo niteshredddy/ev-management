9350e2b41710ef8077eb3b4fb542bd124dc1cf38
niteshredddy <niteshreddy.hyd@gmail.com>
2026-09-18 23:21:16 +0530

    Fix SQLAlchemy pooler freezing

--- backend/app/db.py
+++ backend/app/db.py
@@ -3,13 +3,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from sqlalchemy.pool import NullPool
+
 # If deployed, uses the cloud database. If local, uses gridsync.db
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./gridsync.db")
 # If it's a postgres DB, we shouldn't pass check_same_thread
 connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
 
+engine_kwargs = {"connect_args": connect_args}
+if SQLALCHEMY_DATABASE_URL.startswith("postgresql"):
+    engine_kwargs["poolclass"] = NullPool
+
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args=connect_args
+    SQLALCHEMY_DATABASE_URL, **engine_kwargs
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
